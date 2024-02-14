a4bc6773de31f1ee0518dd2c1b408bcd40ccf072
BenMc123123 <mcdonaldben02@gmail.com>
2024-02-14 12:33:14 +0000

    Added user file upload, waiting for database so can make functional.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,25 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for, send_file
 
-# render map page from templates
+
 @app.route('/')
 def map():
+    """render map page from templates
+
+    Returns:
+        _type_: render template, with map.html
+    """
     return render_template('map.html', title='Map')
+
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    """for user file upload
+
+    Returns:
+        _type_: nothing for now
+    """
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return None
