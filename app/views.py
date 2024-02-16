521d0d9635a0b86f489f212574066a8e35dcd9f8
Fidele Sorgho <sc22f2s@leeds.ac.uk>
2024-02-16 12:43:55 +0000

    Post-Login in progress

--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from app import app
+from flask import Flask, render_template
 
-@app.route('/')
-def index():
-    return "Hello World!!!"
+@app.route('/manager')
+def manager():
+    return render_template("manager.html")
+
+@app.route('/user')
+def user():
+    return render_template("user.html")
