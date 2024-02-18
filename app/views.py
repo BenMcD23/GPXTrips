9b6ccb2351f2a01d1b18dd23be0d456b435b8e8c
Joseph Taylor <josephtaylormtsv@gmail.com>
2024-02-18 18:23:03 +0000

    Built login and registration pages

--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from app import app
+from flask import Flask, render_template
 
-@app.route('/')
-def index():
-    return "Hello World!!!"
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/registration')
+def login():
+    return render_template('registration.html')
