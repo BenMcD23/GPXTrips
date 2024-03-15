bc5c1009fc1b8d7595d37c571dd6a930fc3101d5
BenMc123123 <sc222bm@leeds.ac.uk>
2024-03-15 13:15:34 +0000

    Added label to y axis on future revenue
    Also added some comments to explain the algorithm further

--- app/static/js/revenue.js
+++ app/static/js/revenue.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
     $("select").change(function(){
         $(this).find("option:selected").each(function(){
             var optionValue = $(this).attr("value");
-            console.log(optionValue);
             if(optionValue){
                 $(".chart").not("." + optionValue).hide();
                 $("." + optionValue).show();
